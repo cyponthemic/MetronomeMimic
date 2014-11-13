b557be1d108e5b4c78e42212fae96897890c13b6
Alex <Chavet>
2014-11-14 10:21:14 +1100

    Prep header & frontpage
    Partial SCSS Files + PHP

---
+++ header.php
@@ -0,0 +1,10 @@
+
+<!DOCTYPE HTML>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<title>metronome Agency : Creative Agency Melbourne</title>
+
+<link rel='stylesheet' href='stylesheets/app.css' type='text/css' media='all' />
+
+</head>

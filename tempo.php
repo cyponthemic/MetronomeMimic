49186b922f6e99461aaa6cc68200488a58e00ad5
Alex <Chavet>
2014-11-15 12:47:38 +1100

    Tempo blog
    PHP file and SASS partial

---
+++ tempo.php
@@ -0,0 +1,43 @@
+<?php require('header.php');?>
+	<div id="content" class="tempo">
+	
+				
+		<div class="row">
+			<div class="small-12 medium-9 columns blog ">
+				
+				<div class="large-6 columns post">
+					<div class="inner">
+						<a><img src="http://www.metronome.com.au/wp-content/uploads/2013/11/category-growth.jpg"></a>
+						<h3><a href="#">How category growth can help your brand</a></h3>
+						<p>
+						Bake a bigger pie, get a bigger slice. That’s the intention behind a category growth strategy.</br>
+						<a>Read More</a>
+						</p>
+					</div>
+				</div>
+			
+			
+			
+			
+			
+			
+			
+			
+			</div>
+			
+			<div class="small-hidden medium-3 columns sidebar">
+				<div class="inner">
+				<h4> Archive </h4>
+				<ul class="archive-list"><hr>
+					<li><a>November 2013</a></li><hr>
+					<li><a>December 2013</a></li><hr>
+					<li><a>January 2014</a></li><hr>
+					<li><a>February 2014</a></li><hr>
+					<li><a>March 2014</a></li><hr>
+				</div>
+			</div>
+		</div>
+	
+	
+	</div>
+<?php require('footer.php');?>

a1dcd1ccbf0813b22ad8d52bc63226646db79fd2
Alex <Chavet>
2014-11-15 12:50:10 +1100

    Front-page PHP

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 
 
 <?php require('header.php');?>
+	<div id="content" class="front-page">
+	
+	<div id="mainslider" class="swiper-container">
+		<div class="swiper-wrapper">
+			<!--First Slide-->
+				<div class="swiper-slide"> 
+					<img src="img/Aquila_Laptop.jpg">
+				</div>
+      
+			<!--Second Slide-->
+				<div class="swiper-slide">
+					<img src="img/Aquila_Laptop.jpg">
+				</div>
+      
+			<!--Third Slide-->
+				<div class="swiper-slide"> 
+					<img src="img/Aquila_Laptop.jpg">
+				</div>
+      <!--Etc..-->
+  </div>
+</div>
+
+	
+	
+	</div>
+<?php require('footer.php');?>
+  

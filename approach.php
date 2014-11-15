f79bfd3433c2fb0f3ffa3c7770b0829df5099db7
Alex <Chavet>
2014-11-15 11:30:23 +1100

    Approach page
    PHP file and SASS partial.
    Didn’t have time to insert the icons.

---
+++ approach.php
@@ -0,0 +1,35 @@
+<?php require('header.php');?>
+	<div id="content" class="approach">
+	
+		<header class="row heading">
+			<div class="small-12 columns">
+				<h1>Approach <small>// how we work</small></h1>
+			</div>
+		</header>
+		
+		<div class="row steps">
+			<div class="small-12 columns step ">
+			<div class="inner">
+			<h3>creative : technology : performance</h3>
+			<p>Our culture is built on the values of working together in a rewarding rhythm, and the benefits creative, technology and performance marketing deliver working in harmony.</p>
+			</div>
+			</div>
+			<div class="small-12 columns step ">
+			<div class="inner">
+			<h3>control the tempo</h3>
+			<p>We believe the pace of marketing in today’s landscape is constantly evolving. We believe in controlling the tempo for our clients, and in doing so perfecting both message and budget. We believe in collaborating with clients and consumers, listening to the market – and that the right conversation today will inspire a great idea tomorrow.</p>
+			</div>
+			</div>
+			<div class="small-12 columns step ">
+			<div class="inner">
+			<h3>building successful and sustainable partnershipsw</h3>
+			<p>
+			From the physical environment to online, through traditional and digital channels, our aim is to help clients build their brands by delivering engaging communication that influences, drives consideration, conversion and success. Our philosophy is to build partnerships and always be valued and driven by our natural instinct to be curious.	
+			</p>
+			</div>
+			</div>
+		</div>
+	
+	
+	</div>
+<?php require('footer.php');?>

49ba5a9e64f68b6d9391362b6f965d140eeccab2
Alex <Chavet>
2014-11-15 11:29:15 +1100

    Work page
    PHP file and SASS partial.

---
+++ work.php
@@ -0,0 +1,42 @@
+<?php require('header.php');?>
+	<div id="content" class="work">
+	
+		<header class="row heading">
+			<div class="small-12 columns">
+				<h1>Work <small>//creative portofolio</small></h1>
+			</div>
+		</header>
+		
+		<div class="row projects">
+			<div class="small-12 medium-6 columns project ">
+				<a class="australand"></a>
+				<div class="inner">
+					<h3>AUSTRALAND</h1>
+				</div>
+			</div>
+			
+			<div class="small-12 medium-6 columns project">
+				<a class="mbs" ></a>
+				<div class="inner">
+					<h3>MELBOURNE BUSINESS SCHOOL</h1>
+				</div>
+			</div>
+			
+			<div class="small-12 medium-6 columns project ">
+				<a class="aquila"></a>
+				<div class="inner">
+					<h3>AQUILA</h1>			
+				</div>
+			</div>
+			
+			<div class="small-12 medium-6 columns project ">
+				<a class="alliance"></a>
+				<div class="inner">
+					<h3>ALLIANCE INSURANCE</h1>
+				</div>
+			</div>
+		</div>
+	
+	
+	</div>
+<?php require('footer.php');?>

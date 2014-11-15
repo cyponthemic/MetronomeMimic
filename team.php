834352e12e5352aa2c958048a9f695778d548fe8
Alex <Chavet>
2014-11-15 12:11:02 +1100

    Team page
    PHP file and SASS partial

---
+++ team.php
@@ -0,0 +1,68 @@
+<?php require('header.php');?>
+	<div id="content" class="team">
+	
+		<header class="row heading">
+			<div class="small-12 columns">
+				<h1>Team <small>// agency snapshot</small></h1>
+			</div>
+		</header>
+		
+		<div class="row">
+			<div class="small-12 columns ">
+			<img class="snapshot-team"src="img/metronome_office.jpg">
+			</div>
+		</div>
+		
+		<div class="row team-list">
+			<div class="large-3 columns">
+				<div class="inner">
+					<h3>01.John Smith</h3>
+					<h4>Designer</h4>
+				</div>
+			</div>	
+			<div class="large-3 columns">
+				<div class="inner">
+					<h3>01.John Smith</h3>
+					<h4>Designer</h4>
+				</div>
+			</div>	
+			<div class="large-3 columns">
+				<div class="inner">
+					<h3>01.John Smith</h3>
+					<h4>Designer</h4>
+				</div>
+			</div>	
+			<div class="large-3 columns">
+				<div class="inner">
+					<h3>01.John Smith</h3>
+					<h4>Designer</h4>
+				</div>
+			</div>	
+			<div class="large-3 columns">
+				<div class="inner">
+					<h3>01.John Smith</h3>
+					<h4>Designer</h4>
+				</div>
+			</div>	
+			<div class="large-3 columns">
+				<div class="inner">
+					<h3>01.John Smith</h3>
+					<h4>Designer</h4>
+				</div>
+			</div>	
+			<div class="large-3 columns">
+				<div class="inner">
+					<h3>01.John Smith</h3>
+					<h4>Designer</h4>
+				</div>
+			</div>	
+			<div class="large-3 columns">
+				<div class="inner">
+					<h3>01.John Smith</h3>
+					<h4>Designer</h4>
+				</div>
+			</div>	
+		</div>
+	
+	</div>
+<?php require('footer.php');?>

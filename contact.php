e8fd1a4d898cc38c245ef2f1ed39bf0e95edaa44
Alex <Chavet>
2014-11-15 12:01:41 +1100

    Contact page
    PHP file and SASS partial

---
+++ contact.php
@@ -0,0 +1,26 @@
+<?php require('header.php');?>
+	<div id="content" class="contact">
+	
+		<header class="row heading">
+			<div class="small-12 columns">
+				<h1>Contact <small>//metronome agency</small></h1>
+			</div>
+		</header>
+		
+		<div class="row contact-content">
+			<div class="small-3 columns">
+				<h3>metronome agency</h3>
+				<p>
+				Level 4, 25 Ross Street</br>
+				South Melbourne 3205</br>
+				Victoria, Australia</br>
+				</p>
+			</div>
+			<div class="small-9 columns">
+			<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.2955195367135!2d144.96027410000002!3d-37.82996699999993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d555a5a0575%3A0x762c76721dba2ee8!2s4%2F25+Ross+St%2C+South+Melbourne+VIC+3205!5e0!3m2!1sen!2sau!4v1405639892029" style="border:0"></iframe>
+			</div>
+		</div>
+	
+	
+	</div>
+<?php require('footer.php');?>

0ccd9ddf280dcaebbd2b13a40483b06b3a3c0ad5
Alex <Chavet>
2014-11-15 12:06:32 +1100

    Footer page
    Php files - Loading Jquery & Foundation Js

---
+++ footer.php
@@ -0,0 +1,16 @@
+ <!-- body content here -->
+			</section>
+			
+			
+		  <a class="exit-off-canvas"></a>
+		
+		  </div>
+		</div>
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/foundation/js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+	
+	
+	  </body>
+
+</html>

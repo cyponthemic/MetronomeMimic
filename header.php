9ed3486e937c480339c37f4da76be53ee4714d6d
Alex <Chavet>
2014-11-15 12:51:04 +1100

    Header
    Hedaer PHP file and SASS partial. I use the header partial as a global
    SASS partial.

--- header.php
+++ header.php
@@ -1,10 +1,52 @@
+<!doctype html>
+<html class="no-js" lang="en">
 
-<!DOCTYPE HTML>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+  <head>
+    <meta charset="utf-8" />
+    <!-- if you remove this meta tag, the NSA will spy on you through your Xbox Kinect camera -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Metronome</title>
+    <link rel="stylesheet" href="stylesheets/app.css" />
+    <script src="bower_components/modernizr/modernizr.js"></script>
+    <link rel="stylesheet" href="stylesheets/idangerous.swiper.css">
+	<script defer src="js/idangerous.swiper-2.x.min.js"></script>
+  </head>
 
-<title>metronome Agency : Creative Agency Melbourne</title>
-
-<link rel='stylesheet' href='stylesheets/app.css' type='text/css' media='all' />
-
-</head>
+  <body>
+  
+		<div class="off-canvas-wrap" data-offcanvas>
+		  <div class="inner-wrap">
+		    <nav class="tab-bar sticky">
+		      <section class="left-small">
+		        <a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
+		      </section>
+		
+		      <section class="middle tab-bar-section">
+		        <div class="dots"><img src="http://www.metronome.com.au/wp-content/themes/metronome/img/misc/dots.png">
+			        <h1 class="title">Metronome</h1>
+		        </div>
+		        
+		      </section>
+		
+		      <!--
+<section class="right-small">
+		        <a class="right-off-canvas-toggle menu-icon" href="#"><span></span></a>
+		      </section>
+-->
+		    </nav>
+		
+		    <aside class="left-off-canvas-menu">
+		      <ul class="off-canvas-list">
+		        <li><a href="index.php">home</a></li>
+		        <li><a href="approach.php">approach</a></li>
+		        <li><a href="work.php">work</a></li>
+		        <li><a href="team.php">team</a></li>
+		        <li><a href="#">tempo blog</a></li>
+		        <li><a href="#">now hiring</a></li>
+		        <li><a href="contact.php">contact</a></li>
+		      </ul>
+		    </aside>
+		
+		    <section class="main-section">
+		      <!-- content goes here -->
+		    
